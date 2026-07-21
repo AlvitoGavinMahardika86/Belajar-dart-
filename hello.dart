f9f3978b98492387a7ac5f9ed783afe059bef9d5
Alvito <alvitogavin86@gmail.com>
2026-07-21 08:08:56 +0700

    first commit

---
+++ hello.dart
@@ -0,0 +1,12 @@
+// Ini adalah komentar satu baris.
+
+/*
+  Ini adalah komentar
+  yang bisa lebih dari
+  satu baris.
+*/
+
+void main() {
+  print('Baris ini akan dieksekusi.'); // Komentar juga bisa di akhir baris.
+  // print('Baris ini tidak akan dieksekusi.');
+}
